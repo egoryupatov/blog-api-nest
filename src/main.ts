884fcc806afe9710ebf0641d6bb58edbfe0d47a9
Egor Iupatov <95219188+egoryupatov@users.noreply.github.com>
2023-01-22 16:51:08 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const PORT = process.env.PORT || 3005;
-  const HOST = process.env.HOST || 'https://blogger-server.onrender.com';
-  await app.listen(PORT, HOST);
+  await app.listen(3005);
 }
 bootstrap();
